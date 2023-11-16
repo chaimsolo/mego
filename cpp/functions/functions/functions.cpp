b4f51c6e65b8bbea51a1fa6043ddd75e03920945
chaimsolo2 <chaimsolo2@gmail.com>
2023-11-16 21:55:46 +0200

    number of dividers in c

--- cpp/functions/functions/functions.cpp
+++ cpp/functions/functions/functions.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 
 
@@ -32,12 +33,15 @@
 	for (i = 2; i < num; i++) {
 		if (num % i == 0) {
 			printf("The number %d is not a primary number", num);
-			return num;
+			printf("%d", i);
+			return 1;
 		}
-		printf("The number % d is  a primary number", num);
-		return num;
+		
 		
 	}
+	printf("The number %d is  a primary number", num);
+	printf("%d", i);
+	return 0;
 }
 void main() {
 	int number;
@@ -53,7 +57,7 @@ void main() {
 
 /*long polindromNumber(int num) {
 	int number1 = 0, number2 = num;
-	for (num; num > 0; num % 10) {
+	for (num; num > 0;) {
 		number1 = number1 * 10 + num % 10;
 		num = num / 10;
 	}
@@ -71,3 +75,132 @@ void main() {
 }*/
 
 
+//exersics 35 perfekt number
+
+/*long isPerfect(long num) {
+	
+	long i, sumOfDividers = 0;
+
+	for (i = 1; i < num; i++) {
+		if (num % i == 0) {
+			sumOfDividers += i;
+			printf("%d  ", i);
+		}
+	}
+	if (sumOfDividers == num) {
+		printf("The number %ld is a perfect number.\n", num);
+		return 1;
+	}
+	else
+		printf("the numbr %d is not a perfect number.", num);
+	return 0;
+}
+
+long main() {
+	long number;
+
+	printf("Enter here a number >>> ");
+	scanf_s("%ld", &number);
+	isPerfect(number);
+}*/
+
+/*long main() {
+	long i, number, check;
+
+	printf("Enter a number to chek if ther is a perfect numbers in the range >>> ");
+	scanf_s("%ld", &number);
+	for (i = 1; i <= number; i++) {
+		isPerfect(i);
+	}
+}*/
+
+
+//exersices 34 1089 number
+
+/*int number1089(int num) {
+
+	int i, oppositeNumber = 0, oppositeNumber1 = oppositeNumber, copyNumber = num, num1 = num, check, coppyCheck, oppositeI = 0;
+
+	for (i = 1; i <= 3; i++) {
+		oppositeNumber = oppositeNumber * 10 + copyNumber % 10;
+		copyNumber /= 10;
+	}
+	if (num > oppositeNumber) {
+		check = num - oppositeNumber;
+	}
+	else {
+		check = oppositeNumber - num;
+		num1 = oppositeNumber;
+		oppositeNumber1 = num;
+	}
+	coppyCheck = check;
+	for (i = 1; i <= 3; i++ ) {
+		oppositeI = oppositeI * 10 + coppyCheck % 10;
+		coppyCheck /= 10;
+	}
+	if (check + oppositeI == 1089) {
+		printf("You enterd the number %d, and the opposite of this is %d,\nand %d - %d is %d, and %d + %d is 1089", num, oppositeNumber, num1, oppositeNumber1, check, check, oppositeI);
+	}
+	else
+		printf("The number %d is a polindrom number", num);
+	return 1;
+}
+
+
+int main() {
+
+	int number;
+
+	printf("Enter a number  with 3 digiets >>> ");
+	scanf_s("%d", &number);
+		
+	number1089(number);
+}*/
+
+
+
+/*
+//exersices 36 
+
+#include <stdlib.h>
+#include <time.h>
+int countDividers(int num) {
+	int i, numberOfDividers = 0;
+
+	for (i = 1; i <= num / 2; i++) {
+		if (num % i == 0) {
+			numberOfDividers += 1;
+		}
+
+	}
+	return numberOfDividers;
+}
+
+void checkingMaximumDividers(int max) {
+	int i, maxNumberOfDividers = 0, numberOfDividers, randomNumber, maxRandomNumber = 0;
+	
+
+
+	for (i = 1; i <= 10; i++) {
+		randomNumber = rand() % max + 1;
+		numberOfDividers = countDividers(randomNumber);
+		if (numberOfDividers > maxNumberOfDividers or i == 1) {
+			maxNumberOfDividers = numberOfDividers;
+			maxRandomNumber = randomNumber;
+		}
+		printf("   %d                   %d\n", randomNumber, numberOfDividers);
+	}
+	printf("The number with the maximum dividers is: %d, with %d dividres.", maxRandomNumber, maxNumberOfDividers);
+}
+
+void main() {
+	int number;
+	srand(time(NULL));
+	printf("Enter the range of number that you want to check >>> ");
+	scanf_s("%d", &number);
+	printf("\nnumber          number of dividers\n");
+	
+	checkingMaximumDividers(number);
+
+
+}*/
